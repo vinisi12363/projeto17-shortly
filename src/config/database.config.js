64f012f3b14fc3ae62cd51c774146a4419923ccd
vinisi12363 <viniciuspv.si@gmail.com>
2023-05-22 18:53:18 -0300

    fix: fixing bugs

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -8,5 +8,6 @@ const { Pool } = pg
 const configDatabase = {
     connectionString: process.env.DATABASE_URL
 }
+if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase)
